87b928c77ef9737c442e7f521fbf18485469b95c
francesca <francipeco36@gmail.com>
2023-07-17 16:38:34 +0200

    doing reversed array

--- src/org/java/palindrome/PalindromeWords.java
+++ src/org/java/palindrome/PalindromeWords.java
@@ -1,5 +1,6 @@
 package org.java.palindrome;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PalindromeWords {
@@ -8,9 +9,35 @@ public class PalindromeWords {
         e gli risponde se è una parola palindroma oppure no*/
         Scanner scan = new Scanner(System.in);
 
+        // definisco la variabile parola scelta dall'utente
         System.out.print("Inserisci una parola: ");
         String userWord = scan.nextLine();
-        System.out.println(userWord);
+
+        //creo un array con i caratteri della parola dell'utente
+        char [] userWordArray = userWord.toCharArray();
+        System.out.println(userWordArray);
+
+        //creo un array vuoto dove inserisco i caratteri della parola in ordine inverso
+        char [] reverseArray = new char[userWord.length()];
+
+        boolean isPalindrome = false;
+
+        for (int i=userWordArray.length-1; i>=0; i--){
+            reverseArray = new char[]{userWordArray[i]};
+            System.out.println(Arrays.toString(reverseArray));
+        }
+        System.out.println(Arrays.toString(reverseArray));
+
+       /*
+        if(!isPalindrome) {
+            System.out.println(reverseArray);
+            System.out.println("La parola inserita è palindroma.");
+        } else {
+            System.out.println("La parola inserita non è palindroma.");
+        }*/
+
+
+
 
 
     }
