29b0eaec04b7490c539baa90129c06de4a7c82f0
francesca <francipeco36@gmail.com>
2023-07-17 15:09:22 +0200

    file preparation

---
+++ src/org/java/palindrome/PalindromeWords.java
@@ -0,0 +1,11 @@
+package org.java.palindrome;
+
+public class PalindromeWords {
+    public static void main(String[] args) {
+        /*scrivere un programma che chiede all’utente di inserire una parola
+        e gli risponde se è una parola palindroma oppure no*/
+
+
+    }
+
+}
