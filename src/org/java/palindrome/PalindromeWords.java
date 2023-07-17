5aaae2c0c4fbf6ee1a3800553ca22ba5f9bef8dd
francesca <francipeco36@gmail.com>
2023-07-17 15:24:01 +0200

    user word variable added

--- src/org/java/palindrome/PalindromeWords.java
+++ src/org/java/palindrome/PalindromeWords.java
@@ -1,9 +1,16 @@
 package org.java.palindrome;
 
+import java.util.Scanner;
+
 public class PalindromeWords {
     public static void main(String[] args) {
         /*scrivere un programma che chiede all’utente di inserire una parola
         e gli risponde se è una parola palindroma oppure no*/
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci una parola: ");
+        String userWord = scan.nextLine();
+        System.out.println(userWord);
 
 
     }
